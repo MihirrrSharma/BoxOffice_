8b3c43d01eb89faee7c81726d7ee9ac75b3f9dfb
Mihir Sharma <mihir5julysharma@gmail.com>
2023-05-05 15:55:09 +0530

    first commit

--- src/components/movielist/movieList.js
+++ src/components/movielist/movieList.js
@@ -13,6 +13,10 @@ const MovieList = () => {
             getData()
         }, [])
 
+        useEffect(() =>{
+            getData()
+        }, [type])
+
         const getData = () => {
             fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
            .then(res => res.json())
