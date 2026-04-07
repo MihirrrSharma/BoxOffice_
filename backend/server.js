748704e65624dbe526d463c9294d511b188fa1fb
Mihir Sharma <mihir5julysharma@gmail.com>
2026-04-07 11:30:18 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://your-vercel-app.vercel.app"
-  ],
-  credentials: true
-}));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://box-office-z18g-xr0j-8w54bituw-mihirrrsharmas-projects.vercel.app"
+    ],
+    credentials: true
+  })
+);
+
+app.use(cors());
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
